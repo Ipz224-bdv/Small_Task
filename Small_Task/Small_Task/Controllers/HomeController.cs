41119f80795ec0172408d34915844bc73c6afe81
Денис Бордюг <ipz224_bdv@student.ztu.edu.ua>
2023-03-27 01:41:38 +0300

    Small_Task Done

---
+++ Small_Task/Small_Task/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Small_Task.Models;
+using System.Diagnostics;
+
+namespace Small_Task.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Users user)
+        {
+            if (ModelState.IsValid)
+                return View(user);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Small_Task/Small_Task/Models/Users.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Small_Task.Models
+{
+    public class Users
+    {
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "Name does not exist")]
+        public string UserName { set; get; }
+    }
+}
